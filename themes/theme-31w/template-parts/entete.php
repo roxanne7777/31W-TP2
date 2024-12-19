0bc80e9d54c77906e9863b13706d9a99c9aab5c3
roxanne7777 <0533864@cmontmorency.qc.ca>
2024-12-19 11:08:16 -0500

    ef-rap ajouter template-part entete

---
+++ themes/theme-31w/template-parts/entete.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * template-part pour afficher une entete
+ */
+?>
+
+<section class="entete">
+    <h1><?php the_title(); ?></h1> 
+    <div class="introduction">
+        <p>Découvrez les plus belles destinations à travers le monde, classées par pays. Nous avons soigneusement sélectionné les pays les plus populaires pour vous offrir une expérience unique et inoubliable.</p>
+    </div>
+</section>
--- themes/theme-31w/template-pays.php
+++ themes/theme-31w/template-pays.php
@@ -7,6 +7,7 @@
 
 <main class="principal">
 
+  <?php get_template_part('template-parts/entete'); ?>
   <?php get_template_part('template-parts/galerie'); ?>
 
  
